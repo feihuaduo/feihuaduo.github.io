c1ae9c57a5ba99e8c66bbe3b3c7388655d3f1eb8
千年纹 <1799426163@qq.com>
2018-01-06 15:22:39 +0800

    图片轮播使用淡入淡出

--- js/index.js
+++ js/index.js
@@ -18,9 +18,9 @@ $(function() {
 function banner() {
 	var m = n = 0, count;
 	count = $("#banner_list a").length;
-	$("#banner_list a").hide();
+	$("#banner_list a").css("opacity" , '0');
 	$("#banner_list :eq(" + n * 2 + ")").animate({
-		width : 'toggle'
+		opacity : '1'
 	}, 1500);
 	var t = setInterval(function() {
 		n = (n + 1) % count;
@@ -29,10 +29,10 @@ function banner() {
 	var y=setInterval(function() {
 		if(count==0){
 			count = $("#banner_list a").length;
-			$("#banner_list a").hide();
+			$("#banner_list a").css("opacity" , '0');
 			bannerBind();
 			$("#banner_list :eq(" + n * 2 + ")").animate({
-				width : 'toggle'
+				opacity : '1'
 			}, 1500);
 		}
 		else{
@@ -44,10 +44,10 @@ function banner() {
 			var i = $(this).text() - 1;
 			n = i;
 			$("#banner_list :eq(" + n * 2 + ")").animate({
-				width : 'toggle'
+				opacity : '1'
 			}, 1500);
 			$("#banner_list :eq(" + m * 2 + ")").animate({
-				width : 'toggle'
+				opacity : '0'
 			}, 1500);
 			$("#banner ul :eq(" + n * 2 + ")").css("background-color", '#900');
 			$("#banner ul :eq(" + m * 2 + ")").css("background-color", '#6C6D6E');
