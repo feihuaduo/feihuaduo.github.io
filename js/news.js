721a1e78c64165fbad0e218d49301ae2b90407e5
千年纹 <1799426163@qq.com>
2018-01-08 14:09:25 +0800

    资讯

---
+++ js/news.js
@@ -0,0 +1,52 @@
+$(function() {
+	init();
+	$("#foot a").click(function() {
+		add();
+	});
+});
+
+var page=1;
+
+function init() {
+	Bmob.initialize("ded0beecb60386cf6d1175ab6c800a5a",
+			"0265bad7535c4d46bc55359078f0c001");		
+	add();
+}
+
+function add() {
+	var ZiXun = Bmob.Object.extend("ZiXun");
+	var query = new Bmob.Query(ZiXun);
+	query.descending("createdAt");
+	query.skip((page-1)*5);
+	query.limit(5);
+	query.include("zs_message");
+	query.find({
+		success : function(results) {
+			// 循环处理查询到的数据
+			var div = $("#news ul");
+			var img;
+			var id;
+			var message;
+			for (var i = 0; i < results.length; i++) {
+				object = results[i];
+				message=object.get("zs_message");
+				var url="";
+				for(var j=0; j<message.get("message").length;j++){
+					obj=message.get("message")[j];
+					if(url==""&&obj!=null&&obj.image!=null){
+						url=obj.image._url;
+						break;
+					}
+				}
+				div.append("<li><div class=\"news\"><a href=\"#\">"+
+						  "<span class=\"title\">"+object.get("zs_title")+"</span>"+
+						  "<span class=\"time\">"+object.createdAt+"</span>"+
+						  "<img src=\""+url+"!/fwfh/160x160\"></a></div></li>");
+			}
+		},
+		error : function(error) {
+			alert("查询失败: " + error.code + " " + error.message);
+		}
+	});
+	page++;
+}
