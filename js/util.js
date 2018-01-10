572a0e1bf4e8a4c2e5839e7926d72c202bd8c90c
千年纹 <1799426163@qq.com>
2018-01-10 22:42:33 +0800

    bmob初始化代码混淆加密

--- js/util.js
+++ js/util.js
@@ -5,8 +5,7 @@ function getQueryString(name) {
 }
 
 function bmobInit() {
-	Bmob.initialize("ded0beecb60386cf6d1175ab6c800a5a",
-	"0265bad7535c4d46bc55359078f0c001");
+	eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('1.0("3","2");',4,4,'initialize|Bmob|0265bad7535c4d46bc55359078f0c001|ded0beecb60386cf6d1175ab6c800a5a'.split('|'),0,{}));
 }
 
 function test(obj) {
