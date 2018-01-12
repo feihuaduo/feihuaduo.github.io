db925e6463bd07fa0c79da89228123394eacc84d
千年纹 <1799426163@qq.com>
2018-01-12 17:04:16 +0800

    登录

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 $(function() {
 	initInput();
+	login();
 });
 
 function initInput() {
@@ -8,5 +9,25 @@ function initInput() {
 	});
 	$("#c_btu input").mouseleave(function() {
 		this.style.backgroundColor='#6188DF';
+	});
+}
+
+function login() {
+	$("#cen form").submit(function() {
+		var name=$("#c_name input").val();
+		var password=$("#c_pass input").val();
+		if(name=="" || password=="")
+			alert("不能为空");
+		else{
+			Bmob.User.logIn(name, password, {
+				  success: function(user) {
+					  window.location.href="../index.html";
+				  },
+				  error: function(user, error) {
+					  alert("用户名不存在或密码错误");
+				  }
+			});
+		}
+		return false;
 	});
 }
