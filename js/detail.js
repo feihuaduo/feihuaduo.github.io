f3b4c9bc902e120740bf1bb3e9152e7b46430c81
千年纹 <1799426163@qq.com>
2018-01-17 16:31:59 +0800

    代码优化

--- js/detail.js
+++ js/detail.js
@@ -23,7 +23,8 @@ $(function() {
 					p.set("hfneirong", pingluns[hfs]);
 				p.save(null, {
 				  success: function(gameScore) {
-					  window.location.reload();
+					  bj++;
+					  initTieZi();
 				  },
 				  error: function(gameScore, error) {
 					  alert(error.description);
@@ -47,81 +48,13 @@ var tiezi;
 var floor=1;
 var pingluns;
 var hfs=-1;
+var bj=0;
 
 function init() {
 	bmobInit();	
 	initInput();
 	initLogin();
-
-	var id=getQueryString("id");
-	var Tiezi = Bmob.Object.extend("Tiezi");
-	var query = new Bmob.Query(Tiezi);
-	query.equalTo("objectId", id);
-	query.include("message,tiezuo");
-	query.find({
-		success : function(results) {
-			tiezi=results[0];
-			var user=tiezi.get("tiezuo");
-			
-			max=tiezi.get("pls");
-			pingluns=new Array(max);
-			$("#title").append("<h1>"+tiezi.get("title")+"</h1>");
-			$("#icon").attr("src",getTouXiang(user));
-			$("#name").text(user.get("username"));
-			$("#time").text(tiezi.createdAt);
-			var message=tiezi.get("message");
-			for(var i=0;i<message.length;i++){
-				var meg=message[i];
-				if(meg.image!=null)
-					$("#detail").append("<img src=\""+meg.image._url+"!/fwfh/400x400\">");
-				if(meg.message!=null)
-					$("#detail").append("<p>"+meg.message+"</p>");
-			}
-			
-			addPingLun(tiezi);
-		},
-		error : function(error) {
-			alert("查询失败: " + error.code + " " + error.message);
-		}
-	});
-}
-
-function addPingLun(tiezi) {
-	var PingLun = Bmob.Object.extend("PingLun");
-	query = new Bmob.Query(PingLun);
-	query.equalTo("tiezi", tiezi);
-	query.skip((page-1)*10);
-	query.limit(10);
-	query.include("user,hfneirong");
-	query.find({
-		success : function(results) {
-			for (var i = 0; i < results.length; i++) {
-				var pinglun=results[i];
-				user=pinglun.get("user");
-				var hfneirong="";
-				getTouXiang(user);
-				if(pinglun.get("hfneirong")!=null)
-					hfneirong="<span class=\"rhfmessage\">"+pinglun.get("hfneirong").get("neirong")+"</span>";
-				$("#reply").append("<div class=\"reply\" onClick=\"hf("+(floor-1)+")\">"+
-						"<hr>"+
-						"<img class=\"ricon\" src=\""+getTouXiang(user)+"\">"+
-						"<span class=\"rfloor\">"+floor+"楼</span>"+
-						"<ul>"+
-						"<li><span class=\"rname\">"+user.get("username")+"</span>"+
-						"<li><span class=\"rtime\">"+pinglun.createdAt+"</span>"+
-						"</ul>"+
-						hfneirong+
-						"<span class=\"rmessage\">"+pinglun.get("neirong")+"</span>"
-				);
-				pingluns[floor-1]=pinglun;
-				floor++;
-			}
-		},
-		error : function(error) {
-			alert("查询失败: " + error.code + " " + error.message);
-		}
-	});
-	page++;
+	initTieZi();
 }
 
 function initLogin() {
@@ -154,4 +87,88 @@ function hf(i) {
 		$("#huifu span").text("");
 	else
 		$("#huifu span").text("回复:"+pingluns[hfs].get("neirong"));
+}
+
+function initTieZi() {
+	$("#title").empty();
+	$("#icon").empty();
+	$("#name").text("");
+	$("#time").text("");
+	$("#detail").empty();
+	page=1;
+	max=0;
+	floor=1;
+	hfs=-1;
+	
+	var id=getQueryString("id");
+	var Tiezi = Bmob.Object.extend("Tiezi");
+	var query = new Bmob.Query(Tiezi);
+	query.equalTo("objectId", id);
+	query.include("message,tiezuo");
+	query.find({
+		success : function(results) {
+			tiezi=results[0];
+			var user=tiezi.get("tiezuo");
+			max=tiezi.get("pls");
+			pingluns=new Array(max);
+			$("#title").append("<h1>"+tiezi.get("title")+"</h1>");
+			$("#icon").attr("src",getTouXiang(user));
+			$("#name").text(user.get("username"));
+			$("#time").text(tiezi.createdAt);
+			var message=tiezi.get("message");
+			for(var i=0;i<message.length;i++){
+				var meg=message[i];
+				if(meg.image!=null)
+					$("#detail").append("<img src=\""+meg.image._url+"!/fwfh/400x400\">");
+				if(meg.message!=null)
+					$("#detail").append("<p>"+meg.message+"</p>");
+			}
+			addPingLun();
+		},
+		error : function(error) {
+			alert("查询失败: " + error.code + " " + error.message);
+		}
+	});
+}
+
+function addPingLun() {
+	$("#reply").empty();
+	
+	var PingLun = Bmob.Object.extend("PingLun");
+	query = new Bmob.Query(PingLun);
+	query.equalTo("tiezi", tiezi);
+	query.skip((page-1)*10);
+	query.limit(10);
+	query.include("user,hfneirong");
+	query.find({
+		success : function(results) {
+			for (var i = 0; i < results.length; i++) {
+				var pinglun=results[i];
+				user=pinglun.get("user");
+				var hfneirong="";
+				getTouXiang(user);
+				if(pinglun.get("hfneirong")!=null)
+					hfneirong="<span class=\"rhfmessage\">"+pinglun.get("hfneirong").get("neirong")+"</span>";
+				$("#reply").append("<div class=\"reply\" onClick=\"hf("+(floor-1)+")\">"+
+						"<hr>"+
+						"<img class=\"ricon\" src=\""+getTouXiang(user)+"\">"+
+						"<span class=\"rfloor\">"+floor+"楼</span>"+
+						"<ul>"+
+						"<li><span class=\"rname\">"+user.get("username")+"</span>"+
+						"<li><span class=\"rtime\">"+pinglun.createdAt+"</span>"+
+						"</ul>"+
+						hfneirong+
+						"<span class=\"rmessage\">"+pinglun.get("neirong")+"</span>"
+				);
+				pingluns[floor-1]=pinglun;
+				floor++;
+			}
+			if(bj!=0)
+				location.href = "#foot";
+		},
+		error : function(error) {
+			alert("查询失败: " + error.code + " " + error.message);
+		}
+	});
+	page++;
 }
--- js/index.js
+++ js/index.js
@@ -15,53 +15,32 @@ $(function() {
 	init();
 	banner();
 });
+
+var m = n = 0, count;
+
 function banner() {
-	var m = n = 0, count;
-	count = $("#banner_list a").length;
-	$("#banner_list a").css("opacity" , '0');
-	$("#banner_list :eq(" + n * 2 + ")").animate({
-		opacity : '1'
-	}, 1000);
 	var t = setInterval(function() {
 		n = (n + 1) % count;
 		$("#banner ul :eq(" + n * 2 + ")").trigger("click");
 	}, 6000);
-	var y=setInterval(function() {
-		if(count==0){
-			count = $("#banner_list a").length;
-			$("#banner_list a").css("opacity" , '0');
-			bannerBind();
-			$("#banner_list :eq(" + n * 2 + ")").animate({
-				opacity : '1'
-			}, 1000);
-		}
-		else{
-			clearInterval(y);
-		}
-	}, 100);
-	function bannerBind() {
-		$("#banner li").click(function() {
-			var i = $(this).text() - 1;
-			n = i;
-			$("#banner_list :eq(" + n * 2 + ")").animate({
-				opacity : '1'
-			}, 1000);
-			$("#banner_list :eq(" + m * 2 + ")").animate({
-				opacity : '0'
-			}, 500);
-			$("#banner ul :eq(" + n * 2 + ")").css("background-color", '#900');
-			$("#banner ul :eq(" + m * 2 + ")").css("background-color", '#6C6D6E');
-			m = n;
-		});
-	}
-	bannerBind();
+}
+
+function bannerClick(i) {
+	n = i;
+	$("#banner_list :eq(" + n * 2 + ")").animate({
+		opacity : '1'
+	}, 1000);
+	$("#banner_list :eq(" + m * 2 + ")").animate({
+		opacity : '0'
+	}, 500);
+	$("#banner ul :eq(" + n * 2 + ")").css("background-color", '#900');
+	$("#banner ul :eq(" + m * 2 + ")").css("background-color", '#6C6D6E');
+	m = n;
 }
 
 function init() {
 	bmobInit();
 	initLogin();
-			
-		
 			
 	var GuangGao = Bmob.Object.extend("GuangGao");
 	var query = new Bmob.Query(GuangGao);
@@ -73,7 +52,7 @@ function init() {
 			var div1 = $("#banner ul");
 			var div2 = $("#banner_list");
 			var object = results[0];
-			div1.append("<li class=\"on\"><a href=\"#\">1</a></li>");
+			div1.append("<li class=\"on\" onClick=\"bannerClick("+i+")\"><a href=\"#\">1</a></li>");
 			var a = object.get("gg_tp");
 			div2.append("<a href=\"" + object.get("gg_wz") + "\"><img src=\""
 					+ object.get("gg_tp")._url
@@ -81,17 +60,25 @@ function init() {
 					+ object.get("gg_name") + "\"/></a>");
 			for (var i = 1; i < results.length; i++) {
 				object = results[i];
-				div1.append("<li><a href=\"#\">" + (i + 1) + "</a></li>");
+				div1.append("<li onClick=\"bannerClick("+i+")\"><a href=\"#\">" + (i + 1) + "</a></li>");
 				div2.append("<a href=\"" + object.get("gg_wz")
 						+ "\"><img src=\"" + object.get("gg_tp")._url
 						+ "!/fwfh/900x300\" width=\"900\" height=\"300\" alt=\""
 						+ object.get("gg_name") + "\"/></a>");
 			}
+			
 		},
 		error : function(error) {
 			alert("查询失败: " + error.code + " " + error.message);
 		}
-	});
+	}).then(function(obj) {
+		count = $("#banner_list a").length;
+		$("#banner_list a").css("opacity" , '0');
+		$("#banner_list :eq(" + n * 2 + ")").animate({
+			opacity : '1'
+		}, 1000);
+	}, function(error) {
+	});;
 }
 
 function initLogin() {
@@ -99,7 +86,8 @@ function initLogin() {
 	if (currentUser) {
 		$("#login a").text(currentUser.get("username"));
 		$("#login a").attr("href","html/user.html");
-	} else {
+	}
+	else{
 		$("#login a").text("登录");
 		$("#login a").attr("href","html/login.html");
 	}
