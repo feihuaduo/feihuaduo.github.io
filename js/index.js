6dc27501dd3273f42383a92d96352058347878dc
千年纹 <1799426163@qq.com>
2018-01-24 12:49:12 +0800

    资讯移动到首页

--- js/index.js
+++ js/index.js
@@ -43,6 +43,7 @@ function bannerClick(i) {
 function init() {
 	bmobInit();
 	initLogin();
+	add();
 			
 	var GuangGao = Bmob.Object.extend("GuangGao");
 	var query = new Bmob.Query(GuangGao);
@@ -95,4 +96,61 @@ function initLogin() {
 		$("#login a").text("登录");
 		$("#login a").attr("href","html/login.html");
 	}
+}
+
+var page=1;
+var max=0;
+
+function add() {
+	var ZiXun = Bmob.Object.extend("ZiXun");
+	var query = new Bmob.Query(ZiXun);
+	query.count({
+	  success: function(count) {
+		  max=count;
+	  },
+	  error: function(error) {
+	  }
+	});
+
+	query = new Bmob.Query(ZiXun);
+	query.descending("createdAt");
+	query.skip((page-1)*5);
+	query.limit(5);
+	query.include("zs_message");
+	query.find({
+		success : function(results) {
+			// 循环处理查询到的数据
+			var div = $("#news ul");
+			var img;
+			var id;
+			var message;
+			for (var i = 0; i < results.length; i++) {
+				object = results[i];
+				message=object.get("zs_message");
+				var url="";
+				for(var j=0; j<message.get("message").length;j++){
+					obj=message.get("message")[j];
+					if(url==""&&obj!=null&&obj.image!=null){
+						url=obj.image._url;
+						break;
+					}
+				}
+				div.append("<li>" +
+							"<div class=\"news\" onClick=\"divClick('"+message.id+"')\">" +
+								"<span class=\"title\">"+object.get("zs_title")+"</span>"+
+								"<span class=\"time\">"+object.createdAt+"</span>"+
+								"<img src=\""+url+"!/fwfh/160x160\">" +
+							"</div>"+
+							"</li>");
+			}
+		},
+		error : function(error) {
+			alert("查询失败: " + error.code + " " + error.message);
+		}
+	});
+	page++;
+}
+
+function divClick(id) {
+	window.open("html/detail.html?id="+id);   
 }
